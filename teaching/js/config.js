b8f2b1c951fdb832007f881646ef57721ff0a398
MLAAHL <attendnace634@gmail.com>
2026-03-18 13:10:13 +0530

    Update config.js

--- teaching/js/config.js
+++ teaching/js/config.js
@@ -1,5 +1,3 @@
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
 window.APP_CONFIG = {
-  API_BASE_URL: isLocal ? 'http://localhost:5000/api' : 'https://mlaahl.online/api'
+  API_BASE_URL: 'https://mlaahl.online/api'
 };
